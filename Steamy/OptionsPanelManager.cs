5499eb3d57371c858eeca88176fa340dc2fef484
Asbjørn Hansen <ah@fenris.io>
2016-12-20 22:00:45 +0100

    updated nuget packages to C:S 1.6.1-f2

--- Steamy/OptionsPanelManager.cs
+++ Steamy/OptionsPanelManager.cs
@@ -3,8 +3,8 @@
     using System;
     using System.Collections.Generic;
     using ColossalFramework.PlatformServices;
-    using Infrastructure.Configuration;
     using Infrastructure.UI;
+    using Infrastructure.UI.Configuration;
     using Logger;
 
     public class OptionsPanelManager : IOptionsPanelManager
